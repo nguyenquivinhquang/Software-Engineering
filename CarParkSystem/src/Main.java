7bbcbeeb023e3585ca5ef4982b0f801266b60984
quangnguyencp <nguyenquivinhquang@gmail.com>
2022-12-14 22:29:53 +0700

    delete comment

--- CarParkSystem/src/Main.java
+++ CarParkSystem/src/Main.java
@@ -17,44 +17,7 @@ public class Main{
 //        cps.getAllPermitDetail();
         // getPermit infor
         cps.getAllPermitDetail();
-
-
-
-//        // Add permit failed
-//        System.out.println("-------------------------------------------");
-//        System.out.println("Add permit failed");
-//        cps.addPermit("A04", "B05", "001");
-//        cps.addPermit("A05", "B04", "001");
-//
-//        // Change employee's phone number
-//        System.out.println("-------------------------------------------");
-//        System.out.println("Change employee's phone number");
-//        cps.changeEmployeePhone("001", "44444444");
-//
-//        // Delete an employee
-//        System.out.println("-------------------------------------------");
-//        System.out.println("Delete an employee");
-//        cps.deleteEmployee("001");
-//
-//        // Terminate a permit
-//        System.out.println("-------------------------------------------");
-//        System.out.println("Terminate a permit");
-//        cps.deletePermit("A02");
-//
-//        // Replace a permit
-//        System.out.println("-------------------------------------------");
-//        System.out.println("Replace a permit");
-//        cps.replacePermit("A03", "A05", "B05");
-//
-//        // Get employee's detail based on permit number
-//        System.out.println("-------------------------------------------");
-//        System.out.println("Get employee's detail based on permit number");
-//        cps.getEmployeeDetail("A04");
-//
-//        // Get all permit
-//        System.out.println("-------------------------------------------");
-//        System.out.println("Get all permit");
-//        cps.getAllPermitDetail();
+        
     }   
     
 }
